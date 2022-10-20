0d71ea8fede874f3c1b276468de7757d25ab794c
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-10-20 14:58:40 +0200

    Added scenes and collisions

--- TE21B_TopdownGame/Program.cs
+++ TE21B_TopdownGame/Program.cs
@@ -5,43 +5,78 @@ Raylib.SetTargetFPS(60);
 
 Color hotPink = new Color(255, 105, 180, 255);
 
-Rectangle playerRect = new Rectangle(0, 0, 64, 64);
+
+
 Texture2D avatarImage = Raylib.LoadTexture("avatar.png");
+Texture2D bkgImage = Raylib.LoadTexture("background.png");
+
+Rectangle playerRect = new Rectangle(0, 0, avatarImage.width, avatarImage.height);
+Rectangle trapRect = new Rectangle(700, 500, 64, 64);
 
 float speed = 5.5f;
+
+string currentScene = "start"; // start, game, end
 
 while (Raylib.WindowShouldClose() == false)
 {
   // Logik
 
-  if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+  if (currentScene == "game")
   {
-    playerRect.x += speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+    {
+      playerRect.x += speed;
+    }
+    else if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+    {
+      playerRect.x -= speed;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+    {
+      playerRect.y -= speed;
+    }
+    else if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+    {
+      playerRect.y += speed;
+    }
+  
+    if (Raylib.CheckCollisionRecs(playerRect, trapRect))
+    {
+      currentScene = "end";
+    }
   }
-  else if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+  else if (currentScene == "start")
   {
-    playerRect.x -= speed;
-  }
-  if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
-  {
-    playerRect.y -= speed;
-  }
-  else if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
-  {
-    playerRect.y += speed;
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+    {
+      currentScene = "game";
+    }
   }
 
   // Grafik
   Raylib.BeginDrawing();
   Raylib.ClearBackground(Color.WHITE);
 
-  Raylib.DrawTexture(avatarImage,
-    (int)playerRect.x,
-    (int)playerRect.y,
-    Color.WHITE);
+  if (currentScene == "game")
+  {
+    Raylib.DrawTexture(bkgImage, 0, 0, Color.WHITE);
 
-  // Raylib.DrawRectangleRec(playerRect, hotPink);
-  // Raylib.DrawRectangle(40, 20, 32, 32, hotPink);
+    Raylib.DrawTexture(avatarImage,
+      (int)playerRect.x,
+      (int)playerRect.y,
+      Color.WHITE);
+
+    Raylib.DrawRectangleRec(trapRect, Color.RED);
+  }
+  else if (currentScene == "start")
+  {
+
+    Raylib.DrawText("Press ENTER to start", 50, 560, 50, Color.BLACK);
+  }
+  else if (currentScene == "end")
+  {
+    Raylib.DrawText("GAME OVER", 10, 10, 64, Color.RED);
+  }
 
   Raylib.EndDrawing();
 }
